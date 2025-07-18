c675d75a4dcb0ff84f173129443252f3b02d0f55
i07v <hj507p@outlook.sa>
2025-07-18 20:54:13 +0300

    Update sami.js

--- sami.js
+++ sami.js
@@ -1,7 +1,1 @@
 // قد لا تعمل في كل المتصفحات!
-document.getElementById('myVideo').play().then(() => {
-    this.muted = false;
-    this.volume = 1.0;
-}).catch(error => {
-    console.log("المتصفح منع التشغيل التلقائي:", error);
-});
