302f21cac44955750ce2a12ab20d18fd53df5f82
i07v <hj507p@outlook.sa>
2025-07-18 20:57:48 +0300

    Update sami.js

--- sami.js
+++ sami.js
@@ -1,1 +1,10 @@
-// قد لا تعمل في كل المتصفحات!
+// يحاول إجبار التشغيل حتى مع مقاومة المتصفح
+document.addEventListener('click', () => {
+    const video = document.getElementById('myVideo');
+    video.play().then(() => {
+        video.muted = false;
+        video.volume = 1.0;
+    }).catch(e => {
+        alert("المتصفح منع التشغيل! اضغط موافق ثم انقر على الصفحة.");
+    });
+});
